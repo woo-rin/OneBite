d3c0c5e95aeda1e767d19a412d66081da6b5b1f2
woo-rin <dlqk091028@gmail.com>
2026-01-26 15:34:12 +0900

    카운터 앱 구현

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,15 +1,19 @@
-import Viewer from './components/viwer';
+import Viewer from './components/Viewer';
 import Controller from './components/Controller';
 import './App.css';
 import { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
   const onClickButton = (value) => {
-    setCount(count + value);
+    if (value === 0) {
+      setCount(0);
+    } else {
+      setCount(count + value);
+    }
   };
   return (
     <div className="App">
-      <h1>simple counter</h1>
+      <h1>Simple Counter</h1>
       <section>
         <Viewer count={count} />
       </section>
--- section06/src/components/Controller.jsx
+++ section06/src/components/Controller.jsx
@@ -1,9 +1,10 @@
 const Controller = ({ onClickButton }) => {
   return (
     <div>
-      <button onClick={() => onClickButton(-1)}>-1</button>
-      <button onClick={() => onClickButton(-10)}>-10</button>
       <button onClick={() => onClickButton(-100)}>-100</button>
+      <button onClick={() => onClickButton(-10)}>-10</button>
+      <button onClick={() => onClickButton(-1)}>-1</button>
+      <button className="reset-btn" onClick={() => onClickButton(0)}>Reset</button>
       <button onClick={() => onClickButton(+1)}>+1</button>
       <button onClick={() => onClickButton(+10)}>+10</button>
       <button onClick={() => onClickButton(+100)}>+100</button>
