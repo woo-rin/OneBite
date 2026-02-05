a52c113fa70e7cd1a8628a36452fb0636cd71e83
woo-rin <dlqk091028@gmail.com>
2026-02-05 18:35:03 +0900

    ui구현하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,8 +1,14 @@
+import Header from './components/Header';
+import Editor from './components/Editor';
+import List from './components/List';
+import './App.css';
 function App() {
   return (
-    <>
-      <h1>js</h1>
-    </>
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
   );
 }
 
---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,10 @@
+import './Editor.css';
+const Editor = () => {
+  return (
+    <div className="Editor">
+      <input placeholder="새로운 todo..."></input>
+      <button>추가</button>
+    </div>
+  );
+};
+export default Editor;
