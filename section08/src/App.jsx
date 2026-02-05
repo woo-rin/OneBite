600dd6e22baafa3c5bca41ddcba67f6709a7bc13
woo-rin <dlqk091028@gmail.com>
2026-02-05 18:57:57 +0900

    기능 구현 준비하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,8 +1,30 @@
 import Header from './components/Header';
+import { useState } from 'react';
 import Editor from './components/Editor';
 import List from './components/List';
 import './App.css';
 function App() {
+  const mockData = [
+    {
+      id: 0,
+      isDone: false,
+      content: 'React study',
+      date: new Date().getTime(),
+    },
+    {
+      id: 1,
+      isDone: false,
+      content: 'RUNNING',
+      date: new Date().getTime(),
+    },
+    {
+      id: 0,
+      isDone: false,
+      content: 'study',
+      date: new Date().getTime(),
+    },
+  ];
+  const [todos, SetTodos] = useState(mockData);
   return (
     <div className="App">
       <Header />
