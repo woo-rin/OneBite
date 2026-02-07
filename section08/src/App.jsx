447c5bedc809b3e9a7f0118b411063b7b4b426ec
woo-rin <dlqk091028@gmail.com>
2026-02-07 13:35:21 +0900

    create - 투두 추가하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Editor from './components/Editor';
 import List from './components/List';
 import './App.css';
@@ -18,17 +18,29 @@ function App() {
       date: new Date().getTime(),
     },
     {
-      id: 0,
+      id: 2,
       isDone: false,
       content: 'study',
       date: new Date().getTime(),
     },
   ];
   const [todos, SetTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    SetTodos([newTodo, ...todos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,9 +1,34 @@
+import { useState, useRef } from 'react';
 import './Editor.css';
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState('');
+  const contentRef = useRef('');
+  const onChangeConstent = (e) => {
+    setContent(e.target.value);
+  };
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit();
+    }
+  };
+  const onsubmit = () => {
+    if (content === '') {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent('');
+  };
   return (
     <div className="Editor">
-      <input placeholder="새로운 todo..."></input>
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onChange={onChangeConstent}
+        onKeyDown={onKeyDown}
+        placeholder="새로운 todo..."
+      ></input>
+      <button onClick={onsubmit}>추가</button>
     </div>
   );
 };
