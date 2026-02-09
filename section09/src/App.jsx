a9a56ce6b275f171b823a4f310cb0eb910b18e61
woo-rin <dlqk091028@gmail.com>
2026-02-10 02:59:30 +0900

    투두리스트 업그레이드

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -1,59 +1,80 @@
+import './App.css';
+import { useRef, useState, useReducer } from 'react';
 import Header from './components/Header';
-import { useState, useRef, useReducer } from 'react';
 import Editor from './components/Editor';
 import List from './components/List';
-import Exam from './components/Exam';
-import './App.css';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래 연습하기',
+    date: new Date().getTime(),
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item,
+      );
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const mockData = [
-    {
-      id: 0,
-      isDone: false,
-      content: '리액트 공부 study',
-      date: new Date().getTime(),
-    },
-    {
-      id: 1,
-      isDone: false,
-      content: 'RUNNING',
-      date: new Date().getTime(),
-    },
-    {
-      id: 2,
-      isDone: false,
-      content: 'study',
-      date: new Date().getTime(),
-    },
-  ];
-  const [todos, SetTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  };
 
-    SetTodos([newTodo, ...todos]);
-  };
   const onUpdate = (targetId) => {
-    SetTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo,
-      ),
-    );
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId,
+    });
   };
+
   const onDelete = (targetId) => {
-    SetTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId,
+    });
   };
+
   return (
     <div className="App">
-      <Exam />
-      {/* <Header />
+      <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
