5b2a2258d63253f8c6491d8c935692b6dd3ca750
woo-rin <dlqk091028@gmail.com>
2026-02-14 06:44:49 +0900

    원시타입과 리터럴타입

---
+++ TypeScript/section02/src/chapter1.ts
@@ -0,0 +1,27 @@
+//number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+//string
+let str1: string = 'hello';
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hell0 ${num1}`;
+
+//bollean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+//null
+let null1: null = null;
+//undefiend
+let undefiend1: undefined = undefined;
+
+//리터럴 타입
+//리터럴 -> 값
+let numA: 10 = 10;
