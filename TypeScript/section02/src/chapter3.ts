c0d982165721a9a3c9282d7c051f615e44464040
woo-rin <dlqk091028@gmail.com>
2026-02-15 07:51:05 +0900

    객체

--- TypeScript/section02/src/chapter3.ts
+++ TypeScript/section02/src/chapter3.ts
@@ -1,5 +1,15 @@
 // object
-let user: object = {
+let user: {
+  //객체리터럴 타입
+  id?: number;
+  name: string;
+} = {
   id: 1,
   name: 'woo',
+};
+
+let config: {
+  readonly apikey: string;
+} = {
+  apikey: 'dd',
 };
