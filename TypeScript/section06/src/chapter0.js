1df3ab5c42e83da9c4792372822c04b93ce27e8b
woo-rin <dlqk091028@gmail.com>
2026-02-23 20:45:52 +0900

    리액트 앱 생성 및 초기 설정하기

---
+++ TypeScript/section06/src/chapter0.js
@@ -0,0 +1,48 @@
+/**
+ *
+ */
+
+let StudentA = {
+  naem: 'woo',
+  grade: 'A',
+  age: 18,
+  study() {
+    console.log('study hard');
+  },
+};
+
+class Student {
+  name;
+  grade;
+  age;
+
+  constructor(name, grade, age) {
+    this.name = name;
+    this.gradeq = grade;
+    this.age = age;
+  }
+  study() {
+    console.log('dd');
+  }
+  intrdouce() {
+    console.log(`안뇽하세요 ${this.name}입니다`);
+  }
+}
+
+let studentB = new Student('woo', 'D', 19);
+
+class StudentDeveloper {
+    name;
+    grade;
+    age;
+    favoriteskill;
+
+
+    constructor(name,grade,age,favoriteskill){
+        this.name = name;
+        this.grade = grade;
+        this.age = age;
+        this.favoriteskill =favoriteskill;
+        this.smrmaal debugger, clfsi qjw; Wjdrtmakd d[ wk qlrgid;toString,s tkr d;el]
+    }
+}
