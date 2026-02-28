9a17f2a40ca4dcd68c6bb53e2cb32db9372ebc13
woo-rin <dlqk091028@gmail.com>
2026-03-01 08:18:59 +0900

    shadcn/ui 헥심정리

--- SNS/onebite-exam/src/App.tsx
+++ SNS/onebite-exam/src/App.tsx
@@ -1,10 +1,38 @@
+import { Toast } from "radix-ui";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Textarea } from "./components/ui/textarea";
+import { cn } from "./lib/utils";
+import { toast, Toaster } from "sonner";
 
 function App() {
+  const isActive = false;
   return (
-    <div>
-      <Button>버툰</Button>
+    <div className="p-5">
+      <Toaster></Toaster>
+      <Textarea />
+      <Input placeholder="입렫" />
+      <Button
+        onClick={() => {
+          toast("dd", { position: "bottom-center" });
+        }}
+      >
+        버툰
+      </Button>
+      <Button variant={"destructive"}>no</Button>
+      <Button variant={"ghost"}>no</Button>
+      <Button variant={"link"}>no</Button>
+      <Button variant={"secondary"}>no</Button>
+      <div
+        className={cn(
+          "w-10 text-2xl",
+          isActive ? "text-green-500" : "text-red-500",
+        )}
+      >
+        smrmaaks
+      </div>
+
       <div className="text-primary">dd</div>
       <div className="text-muted">muted</div>
       <div className="text-destructive">ddd</div>
