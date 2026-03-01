de41ccd3d27d4b8a4dbeacc75a9de0964908bdf2
woo-rin <dlqk091028@gmail.com>
2026-03-01 21:58:09 +0900

    React Royter V7 핵심정리

--- SNS/onebite-exam/src/App.tsx
+++ SNS/onebite-exam/src/App.tsx
@@ -1,122 +1,26 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./components/ui/carousel";
-import { Input } from "./components/ui/input";
-import { Textarea } from "./components/ui/textarea";
-import { cn } from "./lib/utils";
-import { toast, Toaster } from "sonner";
-import {
-  Popover,
-  PopoverAnchor,
-  PopoverContent,
-  PopoverTrigger,
-} from "./components/ui/popover";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./components/ui/dialog";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "./components/ui/alert-dialog";
-import { ChefHat } from "lucide-react";
-function App() {
-  const isActive = false;
+import { Outlet, Route, Routes } from "react-router";
+import IndexPage from "./pages/index-page";
+import SignInPage from "./pages/sign-in-page";
+import SignUpPage from "./pages/sign-up-page";
+
+function AuthLayout() {
   return (
-    <div className="p-5">
-      <ChefHat className="h-10 w-10 bg-red-100" />
-      <AlertDialog>
-        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
-
-        <AlertDialogContent>
-          <AlertDialogTitle>Tilw</AlertDialogTitle>
-          <AlertDialogDescription>dddd</AlertDialogDescription>
-          <div>body</div>
-          <div>
-            <AlertDialogAction
-              onClick={() => {
-                console.log("니ㅣ");
-              }}
-            >
-              confrom
-            </AlertDialogAction>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-          </div>
-        </AlertDialogContent>
-      </AlertDialog>
-
-      <Dialog>
-        <DialogTrigger>open Dialog</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>title</DialogTitle>
-            <DialogDescription>Descripton</DialogDescription>
-          </DialogHeader>
-          <div>Bod</div>
-        </DialogContent>
-      </Dialog>
-
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button>ss</Button>
-        </PopoverTrigger>
-        <PopoverContent>dd</PopoverContent>
-      </Popover>
-      <Carousel className="mx-10">
-        <CarouselContent>
-          <CarouselItem className="basis-1/3">w</CarouselItem>
-          <CarouselItem className="basis-1/3">2</CarouselItem>
-          <CarouselItem className="basis-1/3">3</CarouselItem>
-          <CarouselItem className="basis-1/3">4</CarouselItem>
-          <CarouselItem>5</CarouselItem>
-        </CarouselContent>
-        <CarouselNext />
-        <CarouselPrevious />
-      </Carousel>
-      <Toaster></Toaster>
-      <Textarea />
-      <Input placeholder="입렫" />
-      <Button
-        onClick={() => {
-          toast("dd", { position: "bottom-center" });
-        }}
-      >
-        버툰
-      </Button>
-      <Button variant={"destructive"}>no</Button>
-      <Button variant={"ghost"}>no</Button>
-      <Button variant={"link"}>no</Button>
-      <Button variant={"secondary"}>no</Button>
-      <div
-        className={cn(
-          "w-10 text-2xl",
-          isActive ? "text-green-500" : "text-red-500",
-        )}
-      >
-        smrmaaks
-      </div>
-
-      <div className="text-primary">dd</div>
-      <div className="text-muted">muted</div>
-      <div className="text-destructive">ddd</div>
+    <div>
+      <header>Auth!</header>
+      <Outlet />
     </div>
   );
 }
-
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
+  );
+}
 export default App;
