9fb4074314ecd75d9e4d0789420ae57138eaf4f9
woo-rin <dlqk091028@gmail.com>
2026-03-02 23:44:21 +0900

    zustand 미들웨어 1

--- SNS/onebite-exam/src/store/count.ts
+++ SNS/onebite-exam/src/store/count.ts
@@ -1,33 +1,50 @@
-import type { IndentDecrease } from "lucide-react";
 import { create } from "zustand";
+import { Combine } from "lucide-react";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+export const usecountStore = create(
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase: () => {
+          const count = get().count;
+          set({
+            count: count + 1,
+          });
+          set((state) => {
+            state.count += 0;
+          });
+        },
+        decrease: () => {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
 
-export const usecountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      const count = get().count;
-      set({
-        count: count + 1,
-      });
-      set((store) => ({
-        count: store.count,
-      }));
-    },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+// export const usecountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       const count = get().count;
+//       set({
+//         count: count + 1,
+//       });
+//       set((store) => ({
+//         count: store.count,
+//       }));
+//     },
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = usecountStore((Store) => Store.count);
