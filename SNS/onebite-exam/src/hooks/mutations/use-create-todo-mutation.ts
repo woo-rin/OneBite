ce1a2bde6a7475f66e6e701d7d9809153477b690
woo-rin <dlqk091028@gmail.com>
2026-03-08 17:40:58 +0900

    캐시 데이터 2 - 응답 결과값 이용하기

--- SNS/onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
+++ SNS/onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,5 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
@@ -8,12 +9,14 @@ export function useCreateTodoMutation() {
   return useMutation({
     mutationFn: createTodo,
     onMutate: () => {},
-    onSuccess: () => {},
-    onSettled: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSettled: () => {},
+    onSuccess: (newTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     },
+
     onError: (error) => {
       window.alert(error.message);
     },
