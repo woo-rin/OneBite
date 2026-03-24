65ba671aecea4b85084c3e16be5c7e3f7dcb5bcd
woo-rin <dlqk091028@gmail.com>
2026-03-24 20:23:59 +0900

    내 포스트에만 수정/삭제 버튼 렌더링하기

--- SNS/onebite-log/src/components/post/post-item.tsx
+++ SNS/onebite-log/src/components/post/post-item.tsx
@@ -12,6 +12,11 @@ import { useSession } from "@/store/session";
 import DeletePostButton from "./delete-post-button";
 
 export default function PostItem(post: Post) {
+  const session = useSession();
+  const userId = session?.user.id;
+
+  const isMine = post.author_id === userId;
+
   return (
     <div className="flex flex-col gap-4 border-b pb-8">
       {/* 1. 유저 정보, 수정/삭제 버튼 */}
@@ -35,8 +40,12 @@ export default function PostItem(post: Post) {
 
         {/* 1-2. 수정/삭제 버튼 */}
         <div className="text-muted-foreground flex text-sm">
-          <EditPostButton {...post} />
-          <DeletePostButton id={post.id} />
+          (isMine &&(
+          <>
+            <EditPostButton {...post} />
+            <DeletePostButton id={post.id} />
+          </>
+          ))
         </div>
       </div>
 
