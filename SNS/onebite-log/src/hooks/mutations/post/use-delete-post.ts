4ea0578a5a6ab25bb455348c7968913409a646e3
woo-rin <dlqk091028@gmail.com>
2026-03-27 01:11:15 +0900

    캐싱 설정하기 3. 캐시 조작

--- SNS/onebite-log/src/hooks/mutations/post/use-delete-post.ts
+++ SNS/onebite-log/src/hooks/mutations/post/use-delete-post.ts
@@ -1,9 +1,12 @@
 import { deleteImageInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletePost) => {
@@ -12,6 +15,10 @@ export function useDeletePost(callbacks?: UseMutationCallback) {
       if (deletePost.image_urls && deletePost.image_urls.length > 0) {
         await deleteImageInPath(`${deletePost.author_id}/${deletePost.id}`);
       }
+
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
--- SNS/onebite-log/src/hooks/mutations/post/use-update-post.ts
+++ SNS/onebite-log/src/hooks/mutations/post/use-update-post.ts
@@ -1,12 +1,26 @@
 import { updatePost } from "@/api/post";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Post, UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useUpdatePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: updatePost,
-    onSuccess: () => {
+    onSuccess: (updatedPost) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Post>(
+        QUERY_KEYS.post.byId(updatedPost.id),
+        (prevpost) => {
+          if (!prevpost)
+            throw new Error(
+              `${updatedPost.id}에 해당하는 포스트를 캐시에서 찾을수없습니다`,
+            );
+          return { ...prevpost, ...updatedPost };
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
